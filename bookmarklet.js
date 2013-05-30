ca7ccabeecc71bbce2297077c8ca3b0a875c96d7
Morgan Brown <brown.mhg@gmail.com>
2013-05-29 21:57:20 -0700

    Don't put shit in style
    * Use rawgithub to serve the file with the right mime type

--- bookmarklet.js
+++ bookmarklet.js
@@ -7,7 +7,7 @@
 
   if( !window.FuckMyDom ) {
     stag = document.createElement( 'script' );
-    stag.setAttribute( 'src', 'https://raw.github.com/discom4rt/fuck-my-dom/master/fuck-my-dom.js' );
+    stag.setAttribute( 'src', 'https://rawgithub.com/discom4rt/fuck-my-dom/master/fuck-my-dom.js' );
     stag.onload = function(){ FuckMyDom.slowly(); };
     document.body.appendChild( stag );
   } else {
