4de23ce1bc92f076a6923cc1f1675a04d560df79
Morgan Brown <brown.mhg@gmail.com>
2014-10-09 11:42:39 -0400

    Update with new URLs

--- bookmarklet.js
+++ bookmarklet.js
@@ -7,7 +7,7 @@
 
   if( !window.FuckMyDom ) {
     stag = document.createElement( 'script' );
-    stag.setAttribute( 'src', 'https://rawgithub.com/discom4rt/fuck-my-dom/master/fuck-my-dom.js' );
+    stag.setAttribute( 'src', 'http://discom4rt.github.io/fuck-my-dom/fuck-my-dom.js' );
     stag.onload = function(){ FuckMyDom.slowly(); };
     document.body.appendChild( stag );
   } else {
